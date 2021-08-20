13f2761664d6c5cb9ddf3d50898e1ae9e2b4ad34
Avni <avesrastogi@gmail.com>
2021-08-21 01:25:39 +0530

    added js functionality

--- js/main.js
+++ js/main.js
@@ -3,11 +3,31 @@ import GameView from "./GameView.js"
 
 let game = new Game();
 let gameView = new GameView();
+
+// for a new game with class name: restart
+document.querySelector(".restart")
+.addEventListener("click", () => {
+    onRestartClick();
+})
+
+// click on board-tile and should show an X or O on the board-tile
+let tiles = document.querySelectorAll(".board-tile");
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        onTileClick(tile.dataset.index);
+    })
+});
+
+function onTileClick(i) {
+    // make a move - user
+    game.makeMove(i);
+    // update board
+    gameView.updateBoard(game);
+}
+
+function onRestartClick() {
+    game = new Game();
+    gameView.updateBoard(game);
+}
+
 gameView.updateBoard(game);
-
-
-console.log("My turn is", game.turn);
-game.nextTurn();
-console.log("My turn is", game.turn);
-game.makeMove(3);
-console.log(game.board);
